6209322d223b3246fa9d550514a8dc0e1a966820
Dustin Fischer <duasfi@gmail.com>
2021-05-10 18:27:51 +0200

    03: Update, delete views

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api/views.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api/views.py
@@ -1,17 +1,32 @@
+import json
 import os
-import json
-from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
-import json
-from dotenv import load_dotenv
-from flask_cors import CORS
 
-from ..database.models import Drink, db_session
-from .auth import AuthError, requires_auth
+from dotenv import load_dotenv
+from flask import request, jsonify, abort
+from marshmallow import Schema, fields, ValidationError
+
 from . import api
+from .auth import requires_auth
+from ..database.models import Drink, db_session
 
 BASE_DIR = os.path.abspath('.')
 load_dotenv(os.path.join(BASE_DIR, os.getenv('DOT_ENV', '.env')))
+
+
+def validate_positive_int(n):
+    if n <= 0:
+        raise ValidationError("Must be greater than 0.")
+
+
+class RecipeSchema(Schema):
+    name = fields.String(required=True)
+    color = fields.String(required=True)
+    parts = fields.Int(required=True, validate=validate_positive_int)
+
+
+class DrinkSchema(Schema):
+    title = fields.String(required=True)
+    recipe = fields.Nested(RecipeSchema, many=True, required=True)
 
 
 @api.route('/drinks')
@@ -31,86 +46,72 @@ def drinks_detail(_):
     }), 200
 
 
-def validate_required_fields(required_fields):
-    errors = []
-    for field, val in required_fields.items():
-        if val is None:
-            errors.append({field: 'Field is required'})
-
-    if errors:
-        abort(400, errors)
-
-
-def validate_recipe_ingredients_list(ingredients_list):
-    try:
-        ingredient_fields = ('name', 'color', 'parts')
-        if isinstance(ingredients_list, dict):
-            ingredients_list = [ingredients_list]
-        for ingredient in ingredients_list:
-            for field in ingredient_fields:
-                assert bool(ingredient[field])  # strings cant be empty, ints can't be 0, null
-    except KeyError:
-        abort(400, [{'recipe': 'One or more recipe ingredient bodies is malformed'}])
-    except AssertionError:
-        abort(400, [{'recipe': 'All recipe ingredient list field values must be set'}])
-
-
 @api.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def create_drink(_):
     data = request.get_json()
-
-    fields = {
-        'title': data.get('title') or None,
-        'recipe': data.get('recipe') or None
-    }
+    data.pop('id', None)
 
     # Validation
-    validate_required_fields(fields)
-    validate_recipe_ingredients_list(fields['recipe'])
+    try:
+        schema = DrinkSchema()
+        result = schema.load(data)
+    except ValidationError as exc:
+        abort(400, exc.messages)
 
     # Create the drink
-    fields['recipe'] = json.dumps(fields['recipe'])  # JSON serialize recipe
     try:
+        result['recipe'] = json.dumps(result['recipe'])  # JSON serialize recipe
         with db_session():
-            drink = Drink(**fields)
+            drink = Drink(**result)
             drink.insert()
             return jsonify(drink.long())
     except:
         abort(422)
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+@api.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(_, drink_id):
+    # Retrieve the drink, or 404 if not found
+    drink = Drink.query.filter_by(id=drink_id) \
+        .first_or_404('Drink not found matching ID')
+
+    # Parse request data
+    data = request.get_json()
+    data.pop('id', None)
+
+    # Validation
+    try:
+        schema = DrinkSchema(partial=('title', 'recipe'))
+        result = schema.load(data)
+    except ValidationError as exc:
+        abort(400, exc.messages)
+
+    # Update the relevant fields
+    try:
+        if result.get('recipe', None):
+            result['recipe'] = json.dumps(result['recipe'])  # JSON serialize recipe
+        with db_session():
+            # Update the fields
+            for key, value in result.items():
+                setattr(drink, key, value)
+            drink.update()
+            return jsonify(drink.long())
+    except:
+        abort(422)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@api.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(_, drink_id):
+    # Retrieve the drink, or 404 if not found
+    drink = Drink.query.filter_by(id=drink_id) \
+        .first_or_404('Drink not found matching ID')
+    try:
+        with db_session():
+            drink.delete()
+    except:
+        abort(500)
 
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+    return jsonify({}), 204
