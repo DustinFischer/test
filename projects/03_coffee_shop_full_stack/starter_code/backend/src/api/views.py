c6815215c67bd1e0bd4924017e3d22fd5d08c731
Dustin Fischer <duasfi@gmail.com>
2021-05-10 15:39:16 +0200

    03: Drink create view

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api/views.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api/views.py
@@ -1,11 +1,12 @@
 import os
+import json
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-from ..database.models import Drink
+from ..database.models import Drink, db_session
 from .auth import AuthError, requires_auth
 from . import api
 
@@ -28,6 +29,56 @@ def drinks_detail(_):
         'drinks': [drink.long() for drink in Drink.query],
         'success': True
     }), 200
+
+
+def validate_required_fields(required_fields):
+    errors = []
+    for field, val in required_fields.items():
+        if val is None:
+            errors.append({field: 'Field is required'})
+
+    if errors:
+        abort(400, errors)
+
+
+def validate_recipe_ingredients_list(ingredients_list):
+    try:
+        ingredient_fields = ('name', 'color', 'parts')
+        if isinstance(ingredients_list, dict):
+            ingredients_list = [ingredients_list]
+        for ingredient in ingredients_list:
+            for field in ingredient_fields:
+                assert bool(ingredient[field])  # strings cant be empty, ints can't be 0, null
+    except KeyError:
+        abort(400, [{'recipe': 'One or more recipe ingredient bodies is malformed'}])
+    except AssertionError:
+        abort(400, [{'recipe': 'All recipe ingredient list field values must be set'}])
+
+
+@api.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(_):
+    data = request.get_json()
+
+    fields = {
+        'title': data.get('title') or None,
+        'recipe': data.get('recipe') or None
+    }
+
+    # Validation
+    validate_required_fields(fields)
+    validate_recipe_ingredients_list(fields['recipe'])
+
+    # Create the drink
+    fields['recipe'] = json.dumps(fields['recipe'])  # JSON serialize recipe
+    try:
+        with db_session():
+            drink = Drink(**fields)
+            drink.insert()
+            return jsonify(drink.long())
+    except:
+        abort(422)
+
 
 '''
 @TODO implement endpoint
